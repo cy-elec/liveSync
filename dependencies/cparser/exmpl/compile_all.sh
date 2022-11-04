2654ae1817c5905d7b918ecdccc7e2270d722ece
Felix Kröhnert WinWsl <felix.kroehnert@online.de>
2022-11-04 14:53:14 +0100

    added cparser

--- dependencies/cparser/exmpl/compile_all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gcc -o simpleSpace.o simpleSpace.c -l:libcparser.a -L../bin -I../bin
--- dependencies/cparser/exmpl/simpleSpace.c
+++
@@ -1,9 +0,0 @@
-#include "cparser.h"
-
-cparser_dict resolved_args;
-int main(int argc, char *argv[]) {
-	cparser_parseSpace(argc, argv, &resolved_args);
-	cparser_print(fileno(stdout), &resolved_args);
-
-	return 0;
-}
